1cbf494f7d0cc4f1fb4017835b4dd89e9536e282
ErickRamses <105986183+ErickRamses@users.noreply.github.com>
2022-08-07 00:32:27 -0700

    back

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import data from "./data.json"
+
+import b2 from"./assets/destination/background-destination-desktop.jpg"
+
 
 function App() {
+  useEffect(()=>{
+    console.log(data)
+    
+  },[])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <div className='firs'>
+        <div style={{color:"white",maxWidth:"400px",}}>
+
+           <span style={{fontSize:"1.5rem",fontFamily: "Lucida Console, Courier New, monospace"}}>So, you want to travel to</span>
+           <br></br>
+           <span style={{fontSize:"7rem",fontFamily: "Times New Roman, Times, serif"}}>Space</span>
+           <br></br>
+           <br></br>
+
+           Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience! Explore
+        </div>
+
+      </div>
+      <div className="topnav">
+  <a href="#home" onClick={()=>{
+     window.scroll(0,0)
+  }}>Home</a>
+  <a href="#news">Destinations</a>
+  <a href="#contact">Tecnology</a>
+  <a href="#about">About</a>
+</div>
+      <div>
+        Destinations
+        <div style={{height:"100vh",color:"white",backgroundImage:"./assets/destination/background-destination-desktop.jpg"}}>
+          <div>
+            
+            </div>
+          
+          des1{JSON.stringify(data.destinations[0])}</div>
+      </div>
     </div>
   );
 }
